21f4a6998fc1568d5db5692724ae3d1c0745589c
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-02 20:37:22 +0300

    modif 1

--- amazon.js
+++ amazon.js
@@ -7,19 +7,4 @@ const game = () => {
   let moves = 0;
 
   // Function to
-  const playGame = () => {
-    const rockBtn = document.querySelector(".rock");
-    const paperBtn = document.querySelector(".paper");
-    const scissorBtn = document.querySelector(".scissor");
-    const playerOptions = [rockBtn, paperBtn, scissorBtn];
-    const computerOptions = ["rock", "paper", "scissors"];
-
-    // Function to start playing game
-    playerOptions.forEach((option) => {
-      option.addEventListener("click", function () {
-        const movesLeft = document.querySelector(".movesleft");
-        moves++;
-        movesLeft.innerText = `Moves Left: ${10 - moves}`;
-
-        const choiceNumber = Math.floor(Math.random() * 3);
-      }}
+};
