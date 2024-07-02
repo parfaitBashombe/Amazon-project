84a28faebded6c1b19859d7a18e0069f0126f4bb
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-02 20:37:05 +0300

    modif 1

--- amazon.js
+++ amazon.js
@@ -22,18 +22,4 @@ const game = () => {
         movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
         const choiceNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[choiceNumber];
-
-        // Function to check who wins
-        winner(this.innerText, computerChoice);
-
-        // Calling gameOver function after 10 moves
-        if (moves == 10) {
-          gameOver(playerOptions, movesLeft);
-        }
-      });
-    });
-  };
-
-  // Function to decide winner
-};
+      }}
