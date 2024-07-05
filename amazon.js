cbed2a3cf0807cd1835b6153631344620cb910d5
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-05 13:26:35 +0300

    modif 3

--- amazon.js
+++ amazon.js
@@ -1,4 +1,12 @@
 function randonNumber() {
   const number = Math.random();
-  console.log(number);
+  let choice;
+  if (number >= 0 && number < 1 / 3) {
+    choice = "Rock";
+  } else if (number >= 1 / 3 && number < 2 / 3) {
+    choice = "Paper";
+  } else {
+    choice = "Scissors";
+  }
+  console.log(choice);
 }
